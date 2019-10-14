c97c3fbf83729db14f8af2bfce0af96e1cdc743f
youyi <youyi02@meicai.cn>
2019-10-14 11:06:43 +0800

    Iterable阅读，迭代器

--- src/main/java/java/lang/Iterable.java
+++ src/main/java/java/lang/Iterable.java
@@ -42,12 +42,27 @@ import java.util.function.Consumer;
  * @since 1.5
  * @jls 14.14.2 The enhanced for statement
  */
+/*
+ * 内部迭代器，常用作容器类的接口，以支持遍历操作（同时支持流式遍历）
+ *
+ * 内部迭代器的特点是嵌入式
+ * 其迭代行为必须在容器对象内部实现（借助了外部比较器）
+ *
+ * 一个类如果实现了Iterable接口，就意味着“该类本身支持遍历”，并可以通过for-each这种循环语法来直接遍历
+ * 当然，一个类如果没有实现Iterable接口，也可以通过挂载外部迭代器Iterator进行遍历
+ *
+ * 此外，内部迭代器还可转换为可分割迭代器Spliterator，以便用于流式操作
+ *
+ * 注：区别于外部迭代器Iterator
+ * 注：区别于枚举器Enumeration
+ */
 public interface Iterable<T> {
     /**
      * Returns an iterator over elements of type {@code T}.
      *
      * @return an Iterator.
      */
+    // 借助外部迭代器实现遍历
     Iterator<T> iterator();
 
     /**
@@ -69,6 +84,7 @@ public interface Iterable<T> {
      * @throws NullPointerException if the specified action is null
      * @since 1.8
      */
+    // 流式遍历。遍历每个元素，并对其执行相应的择取操作
     default void forEach(Consumer<? super T> action) {
         Objects.requireNonNull(action);
         for (T t : this) {
@@ -97,6 +113,7 @@ public interface Iterable<T> {
      * {@code Iterable}.
      * @since 1.8
      */
+    // 将普通迭代器转换为可分割迭代器，用于流式操作
     default Spliterator<T> spliterator() {
         return Spliterators.spliteratorUnknownSize(iterator(), 0);
     }
